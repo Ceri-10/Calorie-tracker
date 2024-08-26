b143d4a60942a52faa029d95a1ce3e52a4e10aed
Ceri-10 <alberto.canovas@hotmail.com>
2024-08-26 20:50:19 +0200

    #Commit: useState & useReducer

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,45 @@
 import { useState } from "react"
 import { categories } from "../data/categories"
+import { Activity } from "../types"
 
 export default function Form() {
 
-    const [activity,setActivity] = useState({
-        category: "",
+    const [activity,setActivity] = useState<Activity>({
+        category: 1,
         name:"",
         calories:0
     })
  
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>) =>{
+        
+        const isNumberField = ["category", "calories"].includes(e.target.id)
+        
+        setActivity({
+            ...activity,
+            [e.target.id]:isNumberField ? +e.target.value : e.target.value
+        })
+
+    }
+
+    const isValidActivity = () => {
+        const {name,calories} = activity
+        return name.trim() !== "" && calories > 0 
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
 
     return (
-        <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <form className="space-y-5 bg-white shadow p-10 rounded-lg"
+        onSubmit={handleSubmit}>
 
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Categoria:  </label>
                 <select className="border border-slate-300 py-2 rounded-lg w-full bg-white"
                     id="category"
                     value={activity.category}
+                    onChange={handleChange}
                 >
                     {categories.map(category => (
                         <option
@@ -40,6 +62,8 @@ export default function Form() {
                     className="border border-slate-300 py-2 rounded-lg"
                     placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
                     value={activity.name}
+                    onChange={handleChange}
+
                 />
             </div>
 
@@ -52,12 +76,16 @@ export default function Form() {
                     className="border border-slate-300 py-2 rounded-lg"
                     placeholder="Calorias, ej 300 o 500"
                     value={activity.calories}
+                    onChange={handleChange}
+
                 />
             </div>
 
             <input
                 type="submit"
-                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+                value={activity.category === 1 ? "Guardar Comida" : "Guardar Ejercicio"}
+                disabled={!isValidActivity()}
             />
 
         </form>
